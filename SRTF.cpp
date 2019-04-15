cbefc291b79246340044ad219fc6ab03e0ed5254
Ravikant1799 <45918941+Ravikant1799@users.noreply.github.com>
2019-04-16 01:50:59 +0530

    Add files via upload

---
+++ SRTF.cpp
@@ -0,0 +1,78 @@
+#include <stdio.h>
+ 
+  int main()
+{
+  int n,ari[10],bur[10],total=0,i,j,small,temp,procs[100],k,waiting[10],finish[10];
+  float tavg = 0.0, wavg = 0.0;
+  printf("ENTER THE NUMBER OF PROCESSES:");
+  scanf("%d", & n);
+  for (i=0;i<n;i++)
+  {
+    printf("ENTER THE ARRIVAL TIME OF PROCESS %d:\t", i+1);
+    scanf("%d", & ari[i]);
+  }
+  
+  for (i=0;i<n;i++)
+  {
+    printf("ENTER THE BURST TIME OF PROCESS %d:\t", i+1);
+    scanf("%d", & bur[i]);
+    waiting[i] = 0;
+    total+=bur[i];
+  }
+ //sorting on the basis of arrival time
+  for (i=0;i<n;i++)
+  {
+    for (j=i+1;j<n;j++)
+    {
+      if(ari[i]>ari[j])
+      {
+        temp = ari[i];
+        ari[i] = ari[j];
+        ari[j] = temp;
+        temp = bur[i];
+        bur[i] = bur[j];
+        bur[j] = temp;
+      }
+    }
+  }
+ 
+  for (i=0;i<total;i++)
+  {
+    small = 9999;
+    for (j=0;j<n;j++)
+    {
+      if ((bur[j]!=0)&&(ari[j]<=i)&&(bur[j]<small))
+      {
+        small = bur[j];
+        k = j;
+      }
+    }
+ 
+    bur[k]--;
+    procs[i] = k;
+  }
+ 
+  k=0;
+  for (i=0;i<total;i++)
+  {
+    for (j=0;j<n;j++)
+    {
+      if (procs[i]==j)
+      {
+        finish[j]=i;
+        waiting[j]++;
+      }
+    }
+  }
+ 
+ printf("\n PROCESS        COMPLETION TIME         TURNAROUND TIME         WAITING TIME\n");
+  for (i=0;i<n;i++)
+  {
+    printf(" %d\t \t%d \t\t\t%d \t\t\t%d\n",i+1,finish[i]+1,(finish[i]-ari[i])+1,(((finish[i]+1)-waiting[i])-ari[i]));
+    wavg=wavg+(((finish[i]+1)-waiting[i])-ari[i]);
+    tavg=tavg+((finish[i]-ari[i])+1);
+  }
+ 
+  printf("\n Average waiting time==>\t%f", (wavg/n));
+  return 0;
+}

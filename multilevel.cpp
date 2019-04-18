ca3eb17db111b5c9a2ac584f767e2218a899fffa
Ravikant1799 <45918941+Ravikant1799@users.noreply.github.com>
2019-04-19 01:30:52 +0530

    Update multilevel.cpp

--- multilevel.cpp
+++ multilevel.cpp
@@ -3,7 +3,7 @@ int main()
 {
   int count,j,n;
   int time,remaining;
-  int flag=0,time_quantum=10;
+  int flag=0,time_quantum=2;
   int waiting_time=0,turn_around_time=0,arrival_time[10],burst_time[10],rt[10];
   printf("\n\nEnter the Total number of Process:\t ");
   scanf("%d",&n);
